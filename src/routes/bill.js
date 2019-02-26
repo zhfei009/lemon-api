c40d0d734936ffbda7cf0020b5fe66917b20a3d5
zfei <zhfei_cz@163.com>
2019-02-26 22:20:05 +0800

    后台接口完成

--- src/routes/bill.js
+++ src/routes/bill.js
@@ -2,7 +2,7 @@
  * @Author: zFei 
  * @Date: 2019-02-23 10:24:00 
  * @Last Modified by: zFei
- * @Last Modified time: 2019-02-26 16:40:30
+ * @Last Modified time: 2019-02-26 22:14:14
  * @function [账单接口]
  */
 
@@ -17,4 +17,9 @@ router.post('/api/addBill', billAPI.addBill);
 
 /*查看账单接口 */
 router.post('/api/getBill', billAPI.getBill);
+
+/*删除账单接口 */
+router.get('/api/delBill', billAPI.delBill);
+
+
 module.exports = router;
--- src/routes/bill_api/index.js
+++ src/routes/bill_api/index.js
@@ -2,13 +2,14 @@
  * @Author: zFei 
  * @Date: 2019-02-26 16:24:11 
  * @Last Modified by: zFei
- * @Last Modified time: 2019-02-26 16:39:50
+ * @Last Modified time: 2019-02-26 22:17:50
  * @function [账单模块]
  */
 var Mongo = require('Mongodb-curd');
 var batabaseName = 'lemon';
 var collcationName = 'bill'; // 账单集合
 
+var querystring = require('querystring');
 /**
  * [addBill 添加账单]
  */
@@ -44,17 +45,78 @@ function addBill(req, res, next) {
  * [getBill 查看账单]
  */
 function getBill(req, res, next) {
-    var data = req.body;
+    var data = req.body,
+        timer = data.timer, // 时间
+        uID = data.uID, // 用户ID
+        classify = data.classify, // 分类["按摩","餐饮"]
+        query = null, // 条件
+        timeReg = new RegExp('^' + timer); // 年月日正则
+    // console.log(querystring.parse(data.classify));
     // 判断是否缺失参数
-    if (!data.uID) {
+    if (!uID || !timer) {
         res.send({
             code: 3,
             msg: '缺少参数'
         });
         return;
     }
+    // 判断是否只按用户+日期 来查
+    if (!classify) { // 按用户+日期来查
+        query = {
+            "uID": uID,
+            "timer": timeReg
+        };
+    } else { // 按用户+日期+分类来查
+        query = {
+            "uID": uID,
+            "timer": timeReg,
+            "cName": { "$in": classify.split(',') }
+        };
+    }
+
     // 调用mongodb-curd模块下的insert方法
-    Mongo.find(batabaseName, collcationName, data, function(result) {
+    Mongo.find(batabaseName, collcationName, query, function(result) {
+        if (!result) {
+            res.send({
+                code: 0,
+                mes: "error"
+            })
+        } else {
+            if (result.length) {
+                res.send({
+                    code: 1,
+                    mes: "success",
+                    data: result
+                })
+            } else {
+                res.send({
+                    code: 0,
+                    mes: "没有查询到相关信息",
+                    data: result
+                })
+            }
+
+        }
+    })
+
+
+
+}
+
+
+/**
+ * [delBill 删除账单]
+ */
+function delBill(req, res, next) {
+    var id = req.query.id;
+    if (!id) {
+        res.send({
+            code: 3,
+            msg: '缺少参数'
+        });
+        return;
+    }
+    Mongo.remove(batabaseName, collcationName, { "_id": id }, function(result) {
         if (!result) {
             res.send({
                 code: 0,
@@ -63,13 +125,15 @@ function getBill(req, res, next) {
         } else {
             res.send({
                 code: 1,
-                mes: "success",
+                mes: "删除成功",
                 data: result
             })
         }
     })
 }
+
 module.exports = {
     addBill: addBill,
-    getBill: getBill
+    getBill: getBill,
+    delBill: delBill
 }
